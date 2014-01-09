cdc76dc2577f46a51687970ad6d48f7598ee650a
Mori <keitamori@gmail.com>
2014-01-09 14:17:52 +0100

    Ajout d'un module PlayerActions qui gere les interaction du joueur

---
+++ player_actions.rb
@@ -0,0 +1,32 @@
+#Module Player Actions qui se 
+#charge de gerer les actions du
+#joueur et son action avec le tamagotchi
+load "display.rb"
+
+
+module PlayerActions
+    def playerChoice_act_tama(tama,choice)
+#binding.pry
+	if choice ==="0"
+#binding.pry
+		puts"Vous nourrissez votre tama"
+		tama.feed_tama
+
+	    elsif choice === "1"
+		puts "Vous eduquez votre tama"
+		tama.teach_tama
+	    elsif choice === "2"
+		puts "Vous jouez avec votre tama"
+		tama.play_with_tama
+	    elsif   choice === "3"
+		puts "Vous soignez votre tama"
+		tama.cure_tama
+	    elsif choice === "4"
+		puts "Vous nettoyez votre tama"
+		tama.clean_tama
+	    else
+		puts "je n'ai pas compris votre commande"
+	end 
+    end
+
+end
