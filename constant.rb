a93aebb7be169aaf7b3d0b1df71e3c43ea6749c2
Mori <keitamori@gmail.com>
2014-01-13 16:54:49 +0100

    Correction des constantes

--- constant.rb
+++ constant.rb
@@ -1,3 +1,4 @@
+#Variables constantes sur la vie et le mental du tama
 $HEALTH_TAMA=100
 $MENTAL_TAMA=100
 
@@ -5,4 +6,6 @@ $STATE_OF_LIFE=["oeuf","bebe","enfant","adulte","age","mort"]
 
 $MOOD=["heureux","joyeux","malade","neutre","melancolique","depressif","agonisant","reveur"]
 
+#Duree de vie de base du tama en secondes
 $TIME_OF_LIFE=160
+
