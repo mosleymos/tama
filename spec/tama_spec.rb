e6e75a42ba29f739f357e963e8fb519a69a86e15
Mori <keitamori@gmail.com>
2014-01-13 11:15:29 +0100

    Travail sur les test a faire

---
+++ spec/tama_spec.rb
@@ -0,0 +1,23 @@
+describe @tama do
+    describe "#initialize" do
+	it "create a tama object and use it"do 
+	end
+    describe "#feed_tama" do
+	    it "feed your tama"do
+	    end
+	end
+    describe "#play_with_tama" do 
+	    it "play with your current tama"do 
+	    end
+	end
+    describe "#cure_tama" do 
+	    it "cure your tama "do 
+	    end
+	end
+    describe "#teach_tama"do 
+	    it "you teach something " do 
+	    end
+	end
+
+    end
+end 
