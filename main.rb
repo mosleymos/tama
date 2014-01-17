883d8c7f841c990ff05f86b181037f3945a3a87a
KEITA Mori <keitamori@gmail.com>
2014-01-17 18:06:25 +0100

    Adding documentation to the main

--- main.rb
+++ main.rb
@@ -8,6 +8,10 @@ require "chronic"
 include Display
 include PlayerActions
 
+#We're in the main application in the <br>
+#terminal mode
+
+
 BEGIN{
     puts"Lancement du programme  #{$0}"
     puts"Programme se terminera #{$TIME_OF_LIFE} secondes"
@@ -21,8 +25,12 @@ puts "Bienvenue dans l'application tama "
 puts "nous vous invitons a creer un tama"
 puts ban
 
-#On procede a la creation du tama en entrant son nom
-
+#We create our tama with only his name quite coool
+#--
+#We'll think a way to integreate some elements of<br>
+#this main.rb file in tama.rb
+#++
+#We take tama's name with a gets chomp
 nomTama =gets.chomp.to_s
 puts "#{nomTama}"
 
@@ -32,25 +40,33 @@ puts "#{nomTama}"
 tama=Tama.new(nomTama)
 tama.tama_stats(tama) #affichage des stats du tama
 
-#Boucle principale du programme Tama
-#Boucle de la vie
-#Elle se termine lorsque time_to_live=0
+#Main loop of the program<br>
+#It will ask user to make some action on<br>
+#the tama until tama life ends.
+#tama life ends when time_to_live=0
 
 time_to_live = $TIME_OF_LIFE
 
 real_time=Chronic.parse("now")
 time_to_die=real_time
 time_to_die=Chronic.parse("#{$TIME_OF_LIFE} seconds from now")
-
+#--
 #binding.pry
-
 #Risque de boucle infinie le tama dure 120s
-
+#++
 while  (Chronic.parse("now") <=> time_to_die) == -1
 puts banner_tama
-
+#--
 #binding.pry
+#++
 
+#Main logic
+#1.Ask user to enter tama name
+#2.Enter in the loop:
+#-Ask user to make a choice for his tama
+#-The user enter his choice
+#-The choice is made on the tama
+#3.Return to 2 until tama life ends.
 puts"Il lui reste#{real_time} round a vivre"
 puts banner_tama
 tama.tama_stats(tama)
@@ -58,12 +74,18 @@ time_to_live -=1
 tama_actions
 choice_player=gets.chomp
 
+#--
 #binding.pry
+#++
 
 choice_player.to_i
 
+#--
 #binding.pry
+#++
 
+#The main function that's act on the 
+#tama.
 playerChoice_act_tama(tama,choice_player)
 
 sleep 2.5
