4e5f755b14a1efdeb0e0f9789bf5844fe01a93c8
Mori <keitamori@gmail.com>
2014-01-21 18:38:25 +0100

    essai a neuf

--- constant.rb
+++
@@ -1,8 +0,0 @@
-$HEALTH_TAMA=100
-$MENTAL_TAMA=100
-
-$STATE_OF_LIFE=["oeuf","bebe","enfant","adulte","age","mort"]
-
-$MOOD=["heureux","joyeux","malade","neutre","melancolique","depressif","agonisant","reveur"]
-
-$TIME_OF_LIFE=160
