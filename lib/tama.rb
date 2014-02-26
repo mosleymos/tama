73668ad51e1df21e2d84d4cf0a008e00fb7b6e70
Mori <keitamori@gmail.com>
2014-02-26 09:39:36 +0100

    Creation de la base de l'application en cours - ajout de dependances provisoires

--- lib/tama.rb
+++ lib/tama.rb
@@ -1,5 +1,15 @@
 require "tama/version"
+require "tama/constant"
+require "tama/animal_class"
 
 module Tama
   # Your code goes here...
+  class VirtualAnimal<Animal
+    def initialize
+    end
+
+    def main
+
+    end
+  end
 end
