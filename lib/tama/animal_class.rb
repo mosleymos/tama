8e239c05620fec94b5c6f7af93babe429a149e5f
Mori <keitamori@gmail.com>
2014-02-26 13:07:07 +0100

    Ajout des fonctionnalites de notre animal (manger, soigner ...)

--- lib/tama/animal_class.rb
+++ lib/tama/animal_class.rb
@@ -1,19 +1,39 @@
 module Tama
-    class Animal
-        def initialize
-        end
+        class Animal
+            @@nbreAnimal=0
+            @health=0
+            @mental=0
+            def initialize
+                @health=100
+                @mental=100
+                @@nbreAnimal+=1
+                puts "Creation de l'animal \n"
+            end
 
-        def feed
-        end
+            def feed
+                puts"Votre animal se nourrit \n"
+                return setAnimalState(health, mental)
+            end
 
-        def teach
-        end
+            def teach(health, mental)
+                puts"Votre animal apprend quelque chose \n"
+                return setAnimalState(health,mental)
+            end
 
-        def cure
-        end
+            def cure(health, mental)
+                puts"Vous soignez votre animal \n"
+                return setAnimalState(health, mental)
+            end
 
-        def clean
-        end
+            def clean(health, mental)
+                puts"Vous nettoyez votre animal \n"
+                return setAnimalState(health, mental)
+            end
 
+            def setTamaState(health, mental)
+                @health +=health
+                @mental +=mental
+            end
+
+        end
     end
-end
