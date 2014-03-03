88d4a05474bc9a43310f5885c98151f02233d085
Mori <keitamori@gmail.com>
2014-03-03 11:38:49 +0100

    Test en cours de creation - de l'aide svp

---
+++ test/tama/animal_class_test.rb
@@ -0,0 +1,8 @@
+require 'bacon'
+require '/home/simplon/Desktop/TamaApp/tamaGem/tama/lib/tama/animal_class.rb'
+
+describe Animal do 
+
+end 
+
+
---
+++ test/tama_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require '/home/simplon/Desktop/TamaApp/tamaGem/tama/lib/tama.rb'
+
+class VirtualAnimal < Test::Unit::TestCase
+    def test_initialize
+        
+    end
+
+    def test_main
+
+    end
+
+end
