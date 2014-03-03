5135212fc21184d34d997f35f593a3a2295a8148
Mori <keitamori@gmail.com>
2014-03-03 11:37:26 +0100

    ajout des constantes principales - le lien fonctionne

--- lib/tama/constant.rb
+++ lib/tama/constant.rb
@@ -4,4 +4,23 @@ module Tama
 #qui comprendront tout ce que devra etre 
 #statique sur le tama
 
+
+#Constante globale qui determine le temps de vie de tout tamagotchi
+
+    $TIME_OF_LIFE=3600
+
+# Tableau qui determine les etats de vie du tamagotchi
+    
+    $STATE_OF_LIFE=["Oeuf","bebe","enfant","adulte","age","mort"]
+
+#Humeur du tama 
+
+    $MOOD=["heureux","neutre","melancolique","depressif","triste"]
+
+# Un tama ne peut pas etre immortel => cette constante assure ce probleme
+# A reflechir dessus
+
+    $TAMA_MAX_LIFE=9600
+
+
 end
